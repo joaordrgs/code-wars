bb6c2c15938dac5ca9e4207dc7b86a03b50cc491
joaordrgs <joaomanuelribeirorodrigues@gmail.com>
2024-12-28 15:32:06 +0000

    Sums the two smallest integers of a list

--- 7ky/sum_two_lowest_int.py
+++ 7ky/sum_two_lowest_int.py
@@ -6,3 +6,6 @@ For example, when an array is passed like [19, 5, 42, 2, 77], the output should 
 [10, 343445353, 3453445, 3453545353453] should return 3453455.
 '''
 
+def sum_two_smallest_numbers(numbers):    
+    numbers.sort()
+    return numbers[0]+numbers[1] 
