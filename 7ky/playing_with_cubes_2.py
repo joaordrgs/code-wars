70ed21989f6b0f7ec2f4ea807e8a9c181c1c72b9
joaordrgs <joaomanuelribeirorodrigues@gmail.com>
2025-01-07 22:18:14 +0000

    Implements a cube object

---
+++ 7ky/playing_with_cubes_2.py
@@ -0,0 +1,21 @@
+'''
+Hey Codewarrior!
+
+You already implemented a Cube class, but now we need your help again! I'm talking about constructors. We don't have one. Let's code one (or more) such that one can instantiate an object via it, handling either no arguments or a single integer.
+
+Also we got a problem with negative values. Correct the code so negative values will be switched to positive ones!
+
+The constructor taking no arguments should assign 0 to Cube's Side property.
+'''
+
+class Cube(object):
+    def __init__(self, side=0):
+        self.__side = abs(side)
+
+    def get_side(self):
+        """Return the side of the Cube."""
+        return self.__side
+
+    def set_side(self, new_side):
+        """Set the value of the Cube's side, ensuring it's non-negative."""
+        self.__side = abs(new_side)
